59740b69aa29aaf8bd15f64b06ae70d9d019afd4
Ella Williams <MOM@Rashads-iMac.lan>
2016-03-20 22:14:49 -0400

    Added Budget Page with Icons

--- DateNYC/app/src/main/java/com/example/mom/datenyc/YelpAPI/LocationPage.java
+++ DateNYC/app/src/main/java/com/example/mom/datenyc/YelpAPI/LocationPage.java
@@ -1,4 +1,4 @@
-package com.example.mom.datenyc.FourSquareAPI;
+package com.example.mom.datenyc.YelpAPI;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -16,7 +16,6 @@ import com.yelp.clientlib.entities.Business;
 import com.yelp.clientlib.entities.SearchResponse;
 
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,6 +43,10 @@ public class LocationPage extends AppCompatActivity {
     FloatingActionButton mBk, mQu, mMa, mSi;
 
     private String businessId = "joju-elmhurst";
+    static ArrayList<Business> mBusinesses;
+    static String businessName = "";
+    static double rating = 0.0;
+    TextView textView, search;
 
 
     @Override
@@ -52,8 +55,10 @@ public class LocationPage extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("Location");
 
-
+        textView= (TextView)findViewById(R.id.tvMessage);
+        search= (TextView)findViewById(R.id.textView);
         ImageView backgroundTwo = (ImageView) findViewById(R.id.backgroundTwo);
 
         Picasso.with(LocationPage.this).load("https://s-media-cache-ak0.pinimg.com/564x/b6/f1/34/b6f1340783278be2c97535f2674f6f49.jpg").fit().into(backgroundTwo);
@@ -96,18 +101,22 @@ public class LocationPage extends AppCompatActivity {
         // general params
         params.put("term", "food");
         params.put("limit", "5");
-
-
-
+        params.put("category_filter","bars");
 
         Callback<SearchResponse> callback = new Callback<SearchResponse>() {
             @Override
             public void onResponse(Response<SearchResponse> response, Retrofit retrofit) {
                 SearchResponse searchResponse = response.body();
                 // Update UI text with the searchResponse.
-               String bName= searchResponse.businesses().get(0).name();
-                TextView search= (TextView)findViewById(R.id.textView);
-                search.setText(bName);
+               String bName= searchResponse.businesses().get(4).name();
+
+                int totalNumberOfResult = searchResponse.total();
+                mBusinesses = searchResponse.businesses();
+                String businessName = mBusinesses.get(4).name();  // "JapaCurry Truck"
+                Double rating = mBusinesses.get(0).rating();  // 4.0
+
+                search.setText(businessName+ " " +rating);
+
             }
 
             @Override
@@ -128,9 +137,8 @@ public class LocationPage extends AppCompatActivity {
             @Override
             public void onResponse(Response<Business> response, Retrofit retrofit) {
                 Business business = response.body();
-                String name= business.name();
+                String name = business.name();
                 double house= business.rating();
-                TextView textView= (TextView)findViewById(R.id.tvMessage);
 //                textView.setText(String.valueOf(house));
                 textView.setText(name);
                 // Update UI text with the Business object.
@@ -145,5 +153,19 @@ public class LocationPage extends AppCompatActivity {
         call.enqueue(callback);
 
     }
+
+    public void businessInfo(int busNum) throws Exception
+    {
+        // Get name, rating, distance
+        // Display one-by-one in order that Yelp returns data to us
+        if(busNum >= 20) { //TODO: Dont hardcode this. init total results
+            busNum = busNum % 20; //quick wrap around function
+        }
+        businessName = mBusinesses.get(busNum).name();
+        rating = mBusinesses.get(busNum).rating();
+
+
+    }
+
 }
 
